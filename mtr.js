afe7477aad0614bc01702d9bd4e03096cd83b21c
AdBol05 <adam.b@volny.cz>
2022-07-17 16:18:42 +0200

    level definition

--- mtr.js
+++ mtr.js
@@ -15,6 +15,18 @@ module.exports = function(min, max, data) {
     /*var min = Math.min(data);
     var max = Math.max(data);*/
 
+    var L1 = max;
+    var L2 = (max/6)*5;
+    var L3 = (max/6)*4;
+    var L4 = (max/6)*3;
+    var L5 = (max/6)*2;
+    var L6 = max/6;
+    var L7 = min/6;
+    var L8 = (min/6)*2;
+    var L9 = (min/6)*3;
+    var L10 = (min/6)*4;
+    var L11 = (min/6)*5;
+    var L12 = min;
 
     data.forEach( sample => {
         let i = 0;
