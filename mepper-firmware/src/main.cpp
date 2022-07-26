ea91a9cdb4f8c8cf53a712550a93d0e54c0d3beb
AdBol05 <adam.b@volny.cz>
2022-07-26 09:22:02 +0200

    serial input

--- mepper-firmware/src/main.cpp
+++ mepper-firmware/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
+int input = 0;
+
+void mtr(int ID, int Delay){
+  digitalWrite(ID, HIGH);
+  delay(Delay);
+  digitalWrite(ID, LOW);
+  delay(Delay);
+}
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    input = Serial.read();
+    mtr(input, 1);
 }
