78ca4b735eff53e399046615b4d0c4764c7f0d88
AdBol05 <adam.b@volny.cz>
2022-07-26 11:34:47 +0200

    new output type

--- worker.js
+++ worker.js
@@ -3,22 +3,22 @@ module.exports = ({ data, logic }) => {
     let Lout = '';  //temporary section storage
     data.forEach( sample => {//determine section for each sample 
         if(sample > 0){
-            if(sample <= logic.L1 && sample > logic.L2){Lout = '1';}
-            if(sample <= logic.L2 && sample > logic.L3){Lout = '2';}
-            if(sample <= logic.L3 && sample > logic.L4){Lout = '3';}
-            if(sample <= logic.L4 && sample > logic.L5){Lout = '4';}
-            if(sample <= logic.L5 && sample > logic.L6){Lout = '5';}
-            if(sample <= logic.L6 && sample > 0){Lout = '6';}
+            if(sample <= logic.L1 && sample > logic.L2){Lout = 'A';}
+            if(sample <= logic.L2 && sample > logic.L3){Lout = 'B';}
+            if(sample <= logic.L3 && sample > logic.L4){Lout = 'C';}
+            if(sample <= logic.L4 && sample > logic.L5){Lout = 'D';}
+            if(sample <= logic.L5 && sample > logic.L6){Lout = 'E';}
+            if(sample <= logic.L6 && sample > 0){Lout = 'F';}
         }
         if(sample < 0){
-            if(sample >= logic.L7 && sample < 0){Lout = '7';}
-            if(sample >= logic.L8 && sample < logic.L7){Lout = '8';}
-            if(sample >= logic.L9 && sample < logic.L8){Lout = '9';}
-            if(sample >= logic.L10 && sample < logic.L9){Lout = '10';}
-            if(sample >= logic.L11 && sample < logic.L10){Lout = '11';}
-            if(sample >= logic.L12 && sample < logic.L1){Lout = '12';}
+            if(sample >= logic.L7 && sample < 0){Lout = 'G';}
+            if(sample >= logic.L8 && sample < logic.L7){Lout = 'H';}
+            if(sample >= logic.L9 && sample < logic.L8){Lout = 'I';}
+            if(sample >= logic.L10 && sample < logic.L9){Lout = 'J';}
+            if(sample >= logic.L11 && sample < logic.L10){Lout = 'K';}
+            if(sample >= logic.L12 && sample < logic.L1){Lout = 'L';}
         }
-        if(sample === 0) {Lout = '0'}    
+        if(sample === 0) {Lout = 'M'}    
         output.push(Lout);//add resolved section to output table
     });
     return output;
