dfdde00f7a004320e5f6008d94b3fa728d50365f
AdBol05 <adam.b@volny.cz>
2022-09-19 17:48:48 +0100

    map log

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -117,7 +117,7 @@ for(let i in input.pause){pause.push(input.pause[i]);}
 
 for(let i in sequence) {//pin output logic
     //TODO: map input/output
-
+    console.log(notemap.get(sequence[i]));
     console.log("note: " + sequence[i] + " ntm: " + ntm + " timing: " + timing[i]);//debug
     note(ntm, timing[i], m);//call note function with resolved values
     if(pause[i] !== 0){if(pause[i] !== undefined){pa(pause[i]); /*console.log("pause: " + pause[i]);*/}}
