c744ac5a2814eef2c27625fa603f7a09582f53cc
AdBol05 <adam.b@volny.cz>
2022-09-19 18:50:35 +0100

    testing map #9

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -122,7 +122,7 @@ for(let i in sequence) {//pin output logic
     console.log(sequence[i]);
     console.log(tempnote);
     console.log("note: " + sequence[i] + " ntm: " + tempnote.ntm + " motor: " + tempnote.m + " timing: " + timing[i] + "\n");//debug
-    //note(tempnote.ntm, timing[i], tempnote.m);//call note function with resolved values
+    note(tempnote.ntm, timing[i], tempnote.m);//call note function with resolved values
     if(pause[i] !== 0){if(pause[i] !== undefined){pa(pause[i]); /*console.log("pause: " + pause[i]);*/}}
 }
 console.log("\n Done in "+ process.uptime().toFixed(2) + "s \n");//debug
