e1d91ead77cb086162072e42d406c16ab1e4b3bf
AdBol05 <adam.b@volny.cz>
2022-09-23 18:09:49 +0200

    timing bullshit #8

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -88,7 +88,7 @@ async function pa(durp){
 
 //note generation function
 async function note(num, dur, m){
-    del = (num*oct*10000); // /10
+    del = (num*oct); // /10
     coun = Math.floor((dur*5*tempo)/del);
     console.log("Coun: " + coun + " del: " + del + "\n");
     for(let i = 0; i < coun; i++){
