460d451707ded8f20780c5190cce4f9451a72fbc
AdBol05 <adam.b@volny.cz>
2022-09-25 16:30:48 +0100

    midi reader

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //Work in progress -> might be a piece of junk
+const fs = require('fs');
+var parseMidi = require('midi-file').parseMidi
+var writeMidi = require('midi-file').writeMidi
 
 const args = process.argv.slice(2);//process arguments
 var inFile = args[0] //input filename
@@ -13,3 +16,11 @@ console.log('\x1b[32m%s\x1b[0m',"  / __ `__ \\/ _ \\/ __ \\/ __ \\/ _ \\/ ___/ "
 console.log('\x1b[32m%s\x1b[0m'," / / / / / /  __/ /_/ / /_/ /  __/ /          ");
 console.log('\x1b[32m%s\x1b[0m',"/_/ /_/ /_/\\___/ .___/ .___/\\___/_/         ");
 console.log('\x1b[32m%s\x1b[0m',"              /_/   /_/                       \n");
+
+if(args[0] === undefined){console.log('\x1b[31m%s\x1b[0m',"ERROR: Input file path not provided"); process.exit(9);}
+
+
+const input = fs.readFileSync(args[0]);
+var parsed = parseMidi(input)
+
+console.log(parsed.tracks);
