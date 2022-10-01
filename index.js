4c3688eace22a804c0d4c5b4b37f725ceb447436
AdBol05 <adam.b@volny.cz>
2022-10-01 16:39:59 +0100

    cleanup + different package for midi

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 const fs = require('fs');
 const util = require('util');
 
-var parseMidi = require('midi-file').parseMidi;
+const { Midi } = require('@tonejs/midi');
+//var parseMidi = require('midi-file').parseMidi;
 //var writeMidi = require('midi-file').writeMidi
 //var MidiConvert = require('midiconvert');
 
 const args = process.argv.slice(2);//process arguments
 var inFile = args[0] //input filename
 var outFile = inFile.replace(".mid", ".json");//output filename
-var name = ""; //name to be writteno into output file
-if (args[1] === undefined){name = inFile.replace(".mp3", "")}//if not defined, name should be the same as filename of input file
-else{name = args[1];}
+var outnamename = ""; //name to be writteno into output file
+if (args[1] === undefined){outnamename = inFile.replace(".mp3", "")}//if not defined, name should be the same as filename of input file
+else{outnamename = args[1];}
 
 //welcome screen
 console.log('\x1b[32m%s\x1b[0m',"   ____ ___  ___  ____  ____  ___  _____      ");
@@ -22,18 +23,27 @@ console.log('\x1b[32m%s\x1b[0m',"              /_/   /_/                       \
 
 if(args[0] === undefined){console.log('\x1b[31m%s\x1b[0m',"ERROR: Input file path not provided"); process.exit(9);}
 
+const midiData = fs.readFileSync(args[0]);
+const midi = new Midi(midiData);
 
-const input = fs.readFileSync(args[0]);
+console.log(util.inspect(midi, {showHidden: false, depth: 5, colors: true}));
+//console.log(midi)
+
+/*const input = fs.readFileSync(args[0]);
 var parsed = parseMidi(input);
 
-console.log(parsed.tracks);
+console.log(parsed.tracks.length);*/
 //console.log(util.inspect(parsed, {showHidden: false, depth: 3, colors: true}));
 
+
+
 //TBD
+/*
 let notes = [];
 parsed.tracks.forEach( sample =>{
-  notes.push(sample.value);
+  console.log(sample);
+  //notes.push(sample.value);
 });
 
 let dataout = JSON.stringify(notes);
-fs.writeFileSync(outFile, dataout);
+fs.writeFileSync(outFile, dataout);*/
