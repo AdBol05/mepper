5e07ca14612972f5cbd48d2e2a95a4b0b1d54042
AdBol05 <adam.b@volny.cz>
2022-10-09 00:30:54 +0200

    comment

--- raspberry-pi-firmware/multinote.js
+++ raspberry-pi-firmware/multinote.js
@@ -1,5 +1,5 @@
 module.exports = function(parts){
-
-
-
+    //TBD
+    //maybe launch some child processes idk
+    //I have to rotate two motors independently but they can rotate for the same amount of time
 }
