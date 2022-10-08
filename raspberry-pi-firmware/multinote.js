d3a02b12ac7731de645cd23d54a3decbb5c8e3d8
AdBol05 <adam.b@volny.cz>
2022-10-09 00:38:16 +0200

    motor passing

--- raspberry-pi-firmware/multinote.js
+++ raspberry-pi-firmware/multinote.js
@@ -1,4 +1,8 @@
-module.exports = function(parts){
+module.exports = function(parts, M){
+    parts.forEach( parts => {
+        //launch a motor instance for each note simultaneously
+
+    });
     //TBD
     //maybe launch some child processes idk
     //I have to rotate two motors independently but they can rotate for the same amount of time, the speed/delay/frequency is what matters
