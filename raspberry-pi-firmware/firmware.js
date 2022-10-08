51fad4b182d564af84d794a15a7d1570564adfad
AdBol05 <adam.b@volny.cz>
2022-10-08 13:00:49 +0100

    two motors playing one note

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -104,13 +104,18 @@ async function pa(durp){
 
 //note generation function
 async function note(num, dur, m){
+    let n = 1;
+    n = m + 1;
+    if(n > 12){n = n - 12;}
     del = (num*oct); // /10
     coun = Math.floor((dur*5*tempo)/del);
     console.log("Coun: " + coun + " del: " + del + "\n");
     for(let i = 0; i < coun; i++){
         M[m].writeSync(1);
+        M[n].writeSync(1);
         sleep.usleep(del);
         M[m].writeSync(0);
+        M[n].writeSync(0);
         sleep.usleep(del);
     }
 }
