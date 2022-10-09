27d326dc1e6d82a50eda8fc12ddd0392ab82dcfd
AdBol05 <adam.b@volny.cz>
2022-10-09 10:43:57 +0100

    idk what this will do

--- raspberry-pi-firmware/multinote.js
+++ raspberry-pi-firmware/multinote.js
@@ -9,15 +9,15 @@ module.exports = function(parts, notemap, M, timing){
 
     (async function() {
         let i = 0;
-        let result = await Promise.all([
+        //let result = await Promise.all([
             parts.forEach( part => {
                 i++;
                 pool.run(notemap.get(part[i]).ntm, timing, notemap.get(part[i]).m, M, false)
             })
-        ]);
+        //]);
     });
 
     //TBD
-    //maybe launch some child processes idk
+    //maybe launch some child processes
     //I have to rotate two motors independently but they can rotate for the same amount of time, the speed/delay/frequency is what matters
 }
