494e7799119b081570132a7ed090186076fa000b
AdBol05 <adam.b@volny.cz>
2022-10-09 10:40:11 +0100

    trying multithreading

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -149,7 +149,7 @@ for(let i in sequence) {//pin output logic
         let part = sequence[i].split('+');
         console.log("MultiNote: " + part);
         if(pause[i] !== 0 && pause[i] !== undefined){pa(pause[i]);}
-        multinote(part, notemap, M);
+        multinote(part, notemap, M, timing[i]);
     }
     else{
         console.log(sequence[i]);
--- raspberry-pi-firmware/multinote.js
+++ raspberry-pi-firmware/multinote.js
@@ -1,8 +1,22 @@
-module.exports = function(parts, notemap, M){
-    parts.forEach( parts => {
-        //launch a motor instance for each note simultaneously
+const path = require('path');
+const Piscina = require('piscina');
 
+const pool = new Piscina({//worker pool
+    filename: path.resolve(__dirname, 'note.js')
+});
+
+module.exports = function(parts, notemap, M, timing){    
+
+    (async function() {
+        let i = 0;
+        let result = await Promise.all([
+            parts.forEach( part => {
+                i++;
+                pool.run(notemap.get(part[i].ntm, timing, notemap.get(part[i].m, M, false)))
+            })
+        ]);
     });
+
     //TBD
     //maybe launch some child processes idk
     //I have to rotate two motors independently but they can rotate for the same amount of time, the speed/delay/frequency is what matters
