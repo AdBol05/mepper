dcde5b3043a02a59c6937c8ba493fa78706fcf71
AdBol05 <adam.b@volny.cz>
2022-10-09 10:56:54 +0100

    check existence of note in map

--- raspberry-pi-firmware/multinote.js
+++ raspberry-pi-firmware/multinote.js
@@ -8,11 +8,11 @@ const pool = new Piscina({
 module.exports = function(parts, notemap, M, timing){    
 
     (async function() {
-        let i = 0;
         //let result = await Promise.all([
             parts.forEach( part => {
-                i++;
-                pool.run(notemap.get(part[i]).ntm, timing, notemap.get(part[i]).m, M, false)
+                if(notemap.has(part)){
+                    pool.run(notemap.get(part).ntm, timing, notemap.get(part).m, M, false)
+                }
             })
         //]);
     });
