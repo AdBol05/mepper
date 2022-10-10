c01d1e403e8649b4e1093dd2baa769968c5fb484
AdBol05 <adam.b@volny.cz>
2022-10-10 18:37:27 +0100

    wrong file.. smh

--- raspberry-pi-firmware/note.js
+++ raspberry-pi-firmware/note.js
@@ -1,6 +1,17 @@
 module.exports = async function(num, dur, m, M, dual){
     var Gpio = require('onoff').Gpio;
     var sleep = require('sleep');
+
+    /*for(let i = 1; i <= 12; i++){
+        console.log("\nInitializing motor #" + i + " on pin " + pinout[i -1] + ":");
+        M[i] = new Gpio(pinout[i - 1], 'out');
+        //M[i].writeSync(1);
+        //sleep.msleep(100);
+        //M[i].writeSync(0);
+        //sleep.msleep(100);
+        console.log(M[i]);
+    }*/
+
     if(dual){
         let n = 1;
         n = m + 1;
