3a3587fa0dba0fee3db820a8a605094828ffafd5
AdBol05 <adam.b@volny.cz>
2022-10-11 16:00:47 +0200

    trying something

--- raspberry-pi-firmware/note.js
+++ raspberry-pi-firmware/note.js
@@ -1,6 +1,17 @@
-module.exports = async function(num, dur, m, M, dual){
+let M = {};
+let pinout = [14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21];
+
+module.exports = async function(num, dur, m, /*M,*/ dual){
     var Gpio = require('onoff').Gpio;
     var sleep = require('sleep');
+
+    if(dual){
+        let n = m + 1;
+        if(n > 12){n = n - 12;}
+    }
+
+    M[m] = new Gpio(pinout[m - 1], 'out');
+    if(dual){}
 
     /*for(let i = 1; i <= 12; i++){
         console.log("\nInitializing motor #" + i + " on pin " + pinout[i -1] + ":");
@@ -12,11 +23,6 @@ module.exports = async function(num, dur, m, M, dual){
         console.log(M[i]);
     }*/
 
-    if(dual){
-        let n = 1;
-        n = m + 1;
-        if(n > 12){n = n - 12;}
-    }
     del = (num*oct); // /10
     coun = Math.floor((dur*5*tempo)/del);
     console.log("Coun: " + coun + " del: " + del + "\n");
@@ -35,5 +41,6 @@ module.exports = async function(num, dur, m, M, dual){
         M[i].unexport();
     }
     */
+   M[m].unexport();
    
 }
