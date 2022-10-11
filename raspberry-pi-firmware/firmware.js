26b20000d5cf35fbd7bf8000fe640d945e844d03
AdBol05 <adam.b@volny.cz>
2022-10-11 16:23:42 +0200

    dedicated note file

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -155,7 +155,7 @@ for(let i in sequence) {//pin output logic
         //console.log(sequence[i]);
         //console.log(notemap.get(sequence[i]));
         if(notemap.has(sequence[i])){
-            //console.log("note: " + sequence[i] + " ntm: " + notemap.get(sequence[i]).ntm + " motor: " + notemap.get(sequence[i]).m + " timing: " + timing[i]);//debug
+            console.log("note: " + sequence[i]/* + " ntm: " + notemap.get(sequence[i]).ntm + " motor: " + notemap.get(sequence[i]).m + " timing: " + timing[i]*/);//debug
             note(notemap.get(sequence[i]).ntm, timing[i], notemap.get(sequence[i]).m, /*M,*/ true);//call note function with resolved values
             if(pause[i] !== 0 && pause[i] !== undefined){pa(pause[i]);}
         }
