f1dc1384369e73aa347cbd0d773d5d833c48b8ff
AdBol05 <adam.b@volny.cz>
2022-10-12 07:25:49 +0200

    testing multinote #28

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -140,7 +140,7 @@ for(let i in sequence) {//pin output logic
     else{
         if(notemap.has(sequence[i])){
             console.log("note: " + sequence[i]/* + " ntm: " + notemap.get(sequence[i]).ntm + " motor: " + notemap.get(sequence[i]).m + " timing: " + timing[i]*/);//debug
-            note({num: notemap.get(sequence[i]).ntm, dur: timing[i], m: notemap.get(sequence[i]).m, dual: true});//call note function with resolved values
+            note(notemap.get(sequence[i]).ntm, timing[i], notemap.get(sequence[i]).m, true);//call note function with resolved values
             if(pause[i] !== 0 && pause[i] !== undefined){pa(pause[i]);}
         }
     }
