1dad02dd313a0a4e43f6272ff32b3b4385b26123
AdBol05 <adam.b@volny.cz>
2022-10-15 17:01:50 +0100

    TODO: fix multinote timing

--- raspberry-pi-firmware/worker.js
+++ raspberry-pi-firmware/worker.js
@@ -1,6 +1,6 @@
 module.exports = async ({ num, dur, m, dual }) => {
         var note = require('./note.js');
-        await note(num, dur, m, dual);
+        note(num, dur, m, dual);
 
         return "played note: " + num;
     };
