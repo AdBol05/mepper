a7b61a208bf6fd70f55fbd42a235d21695bdb63d
AdBol05 <adam.b@volny.cz>
2022-10-16 11:37:14 +0100

    timing nonsense #38

--- raspberry-pi-firmware/worker.js
+++ raspberry-pi-firmware/worker.js
@@ -1,8 +1,8 @@
 module.exports = async ({ num, dur, m, dual }) => {
     var note = require('./note.js');
     //await note(num, dur, m, dual);
-    (function() {
-        note(num, dur, m, dual);
+    (async function() {
+        await note(num, dur, m, dual);
     })();
 
     //const promise = new Promise (note(num, dur, m, dual));
