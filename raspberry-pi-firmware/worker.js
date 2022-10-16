ebe73b9edbdfb7ab6a5a3d2a0b17f7f2b0f096e0
AdBol05 <adam.b@volny.cz>
2022-10-16 09:21:45 +0100

    timing nonsense #20

--- raspberry-pi-firmware/worker.js
+++ raspberry-pi-firmware/worker.js
@@ -1,9 +1,10 @@
 module.exports = ({ num, dur, m, dual }) => {
     var note = require('./note.js');
     let out = note(num, dur, m, dual).then((out) => {
-        return out;
+        //return out;
+        Promise.resolve(out);
     });
     //console.log("ntm: " + num + " motor: " + m + " timing: " + dur);
-    Promise.resolve(out);
+    //Promise.resolve(out);
     
 };
