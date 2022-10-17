150eb537176563e6fe6304201849973528fa78c8
AdBol05 <adam.b@volny.cz>
2022-10-17 18:20:10 +0100

    multithread #18

--- raspberry-pi-firmware/worker.js
+++ raspberry-pi-firmware/worker.js
@@ -2,7 +2,7 @@ module.exports = ({ num, dur, m, dual }) => {
     const note = require('./note.js');
 
     note(num, dur, m, dual).then(d => {
-        console.log(d);
+        console.log("Petrolej", d);
         return d;
     });
 
