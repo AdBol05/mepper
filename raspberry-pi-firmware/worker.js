5578b1d9e90a5678ef663bde7c94b2682554e0f6
AdBol05 <adam.b@volny.cz>
2022-10-17 16:57:41 +0100

    multithread #1

--- raspberry-pi-firmware/worker.js
+++ raspberry-pi-firmware/worker.js
@@ -1,12 +1,42 @@
 const { Worker, isMainThread, MessageChannel, MessagePort, parentPort } = require("worker_threads");
-
+const Gpio = require('onoff').Gpio;
+const sleep = require('sleep');
 
 parentPort.once("message", (value) => {
 
 
     parentPort.postMessage("AAAAAAAAA")
 
-    const note = require(__dirname + '/note.js');
-    note(value.num, value.dur, value.m, value.dual);
+    global.M = {};
+    let pinout = [14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21];
+    
+    let oct = 5;
+    let coun;
+    let del;
+    let tempo = 120;
+
+    let n = m + 1;
+    if(n > 12){n = n - 12;}
+
+    M[value.m] = new Gpio(pinout[value.m - 1], 'out');
+    if(value.dual){M[n] = new Gpio(pinout[value.m], 'out');}
+
+    console.log("ntm: " + value.num + " motor: " + value.m + " timing: " + value.dur);
+    del = (value.num*oct);
+    coun = Math.floor((value.dur*5*tempo)/del);
+    //console.log("Coun: " + coun + " del: " + del + "\n");
+    for(let i = 0; i < coun; i++){
+        M[value.m].writeSync(1);
+        if(value.dual){M[n].writeSync(1);}
+        sleep.usleep(del);
+        M[value.m].writeSync(0);
+        if(value.dual){M[n].writeSync(0);}
+        sleep.usleep(del);
+    }
+
+    if(value.dual){M[n].unexport();}
+    M[value.m].unexport();
+    //return "ntm: " + value.num + " motor: " + m + " timing: " + value.dur;
+    Promise.resolve(value.num);
 
 })
