da6b342380d9a129d0003314ea5a8d37bce3a51e
AdBol05 <adam.b@volny.cz>
2022-10-17 16:42:47 +0100

    multithread #6

--- raspberry-pi-firmware/worker.js
+++ raspberry-pi-firmware/worker.js
@@ -1,15 +1,15 @@
 const { Worker, isMainThread, MessageChannel, MessagePort, parentPort } = require("worker_threads");
 
-module.exports = ({ num, dur, m, dual }) => {
 
-    parentPort.on("message", (value) => {
+parentPort.on("message", (value) => {
 
-        console.log(value);
-        parentPort.postMessage("AAAAAAAAA")
+    console.log(value);
+    parentPort.postMessage("AAAAAAAAA")
 
-    })
+})
 
-    const note = require('./note.js');
-    note(num, dur, m, dual);
-    Promise.resolve(num);
-};
+console.log("debug");
+
+const note = require('./note.js');
+note(num, dur, m, dual);
+Promise.resolve(num);
