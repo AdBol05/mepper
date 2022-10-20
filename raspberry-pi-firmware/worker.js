098fe4189cbc0cf2849b9d33945d926702482f76
AdBol05 <adam.b@volny.cz>
2022-10-20 17:42:28 +0100

    testing events #32

--- raspberry-pi-firmware/worker.js
+++ raspberry-pi-firmware/worker.js
@@ -5,13 +5,13 @@
 };*/
 module.exports = async ({ num, dur, m, dual }) => {
 
-    return await new Promise(async resolve => {
+    //return await new Promise(async resolve => {
         const note = require('./note.js');
 
         console.log("exec");
 
-        resolve(await note(num, dur, m, dual))
-    })
+        //resolve(await note(num, dur, m, dual))
+    //})
 
 
 };
