d1c12d243b7a05ac51befdc1aaf84b1bc3fa8d98
AdBol05 <adam.b@volny.cz>
2022-10-20 17:25:34 +0100

    testing events #24

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -160,6 +160,7 @@ for(let i in sequence) {//pin output logic
             //(async function(){
                 nt1.emit('note', pool_num1, pool_timing, pool_m1, false);
                 nt2.emit('note', pool_num2, pool_timing, pool_m2, false);
+                sleep.usleep(pool_timing);
             //})();
             //sleep.usleep(pool_timing);
             //async function mltnt() {//TODO: fix promise pending
