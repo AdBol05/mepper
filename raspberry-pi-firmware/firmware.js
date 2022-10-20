19fa12655be1dd2829309163506cf815db7e8327
AdBol05 <adam.b@volny.cz>
2022-10-20 19:12:32 +0100

    output

--- raspberry-pi-firmware/firmware.js
+++ raspberry-pi-firmware/firmware.js
@@ -201,6 +201,7 @@ for(let i in sequence) {//pin output logic
             console.log("note: " + sequence[i]/* + " ntm: " + notemap.get(sequence[i]).ntm + " motor: " + notemap.get(sequence[i]).m + " timing: " + timing[i]*/);//debug
             note(notemap.get(sequence[i]).ntm, timing[i], notemap.get(sequence[i]).m, true);//call note function with resolved values
             if(pause[i] !== 0 && pause[i] !== undefined){pa(pause[i]);}
+            console.log("=============================================================");
         }
     }
 }
