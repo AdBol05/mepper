7637a2371cd94bebc87747566dd7d11cb4b2a0ac
AdBol05 <adam.b@volny.cz>
2022-10-21 20:22:51 +0100

    threaded motors

---
+++ raspberry-pi-firmware/firmwareV2/m2.js
@@ -0,0 +1,23 @@
+module.exports = ({num, dur}) => {
+    let Gpio = require('onoff').Gpio;
+    let sleep = require('sleep');
+
+    let M = new Gpio(15, 'out');
+
+    let oct = 5;
+    let coun;
+    let del;
+    let tempo = 120;
+
+    del = (num * oct);
+        coun = Math.floor((dur * 5 * tempo) / del);
+        for (let i = 0; i < coun; i++) {
+            M[m].writeSync(1);
+            if (dual) { M[n].writeSync(1); }
+            sleep.usleep(del);
+            M[m].writeSync(0);
+            if (dual) { M[n].writeSync(0); }
+            sleep.usleep(del);
+        }
+    M.unexport();
+}
