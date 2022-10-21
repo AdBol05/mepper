921cb6a04853f18acf64af7708749d77ff8dd070
AdBol05 <adam.b@volny.cz>
2022-10-21 21:00:20 +0100

    threaded firmware #8

--- raspberry-pi-firmware/firmwareV2/firmware.js
+++ raspberry-pi-firmware/firmwareV2/firmware.js
@@ -99,6 +99,10 @@ async function pa(durp){
     sleep.msleep(ker);
 }
 
+async function pause(duration){
+    sleep.usleep(duration);
+}
+
 //print basic info (mostly for debugging)
 console.log("\n");
 console.log("==================");
@@ -135,7 +139,7 @@ for(let i in sequence) {//pin output logic
         if(notemap.has(sequence[i])){
             console.log("note: " + sequence[i]/* + " ntm: " + notemap.get(sequence[i]).ntm + " motor: " + notemap.get(sequence[i]).m + " timing: " + timing[i]*/);//debug
             M[notemap.get(sequence[i]).m](notemap.get(sequence[i]).ntm, timing[i]);
-            pa(timing[i]);
+            pause(timing[i]);
             if(pause[i] !== 0 && pause[i] !== undefined){pa(pause[i]);}
         }
     }
