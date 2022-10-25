1d8a93236ea81f37ee58f55a9fda9292196eece2
AdBol05 <adam.b@volny.cz>
2022-10-25 14:43:57 +0100

    promise testing

--- raspberry-pi-firmware/firmwareV2/firmware.js
+++ raspberry-pi-firmware/firmwareV2/firmware.js
@@ -134,7 +134,7 @@ for(let i in sequence) {//pin output logic
                 M[pool_m1]({num: pool_num1, dur: timing[i]});
                 M[pool_m2]({num: pool_num2, dur: timing[i]});
             });
-            pause(timing[i]);
+            pa(timing[i]);
         }
     }
     else{
