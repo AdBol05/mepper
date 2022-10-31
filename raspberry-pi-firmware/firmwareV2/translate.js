ecdfd0ee9d69bc5d2c473e14bd06ee3530ac73f8
AdBol05 <adam.b@volny.cz>
2022-10-31 17:23:32 +0000

    moved declarations

--- raspberry-pi-firmware/firmwareV2/translate.js
+++ raspberry-pi-firmware/firmwareV2/translate.js
@@ -72,10 +72,6 @@ module.exports = (input) => {
     });
 
     for(let i in input.sequence){
-        let delay = Math.floor(notemap.get(input.sequence[i]).ntm * oct);
-        let count = Math.floor((input.timing[i] * 5 * tempo) / delay);
-        let motor = notemap.get(input.sequence[i]).m;
-
         if(input.sequence[i].includes("+")){
             //do some stuff
             let samples = input.sequence[i].split("+");
@@ -84,6 +80,10 @@ module.exports = (input) => {
             */
         }
         else{
+            let delay = Math.floor(notemap.get(input.sequence[i]).ntm * oct);
+            let count = Math.floor((input.timing[i] * 5 * tempo) / delay);
+            let motor = notemap.get(input.sequence[i]).m;
+
             for(let j = 0; j < count; j++){//TODO: figure out how to translate more notes to be played at once
                 data.motor.push(motor);
                 data.action.push(1);
