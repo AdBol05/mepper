757a950564a616f47ec0cf1b5944b58a7d873028
AdBol05 <adam.b@volny.cz>
2022-11-04 21:19:24 +0100

    bar nonsense

--- raspberry-pi-firmware/firmwareV2/translate.js
+++ raspberry-pi-firmware/firmwareV2/translate.js
@@ -141,7 +141,7 @@ module.exports = (input) => {
                 else{data.delay.push(delay);}
             }
         }
-        console.log(i + 1);
+        console.log(Number(i) + 1);
         bar.update(i);
     }
 
