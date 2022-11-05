71c324e50c8067d68ae0dedf4f3e8d9bc9cbffe4
AdBol05 <adam.b@volny.cz>
2022-11-05 12:34:21 +0100

    debug

--- raspberry-pi-firmware/firmwareV2/translate.js
+++ raspberry-pi-firmware/firmwareV2/translate.js
@@ -92,7 +92,8 @@ module.exports = (input) => {
             let delay2 = Math.floor(notemap.get(sample[1]).ntm * oct);
 
             let smallerDelay = Math.floor(smaller(delay1, delay2));
-            let largerDelay = Math.floor(larger(delay1, delay2));
+            let largerDelay = Math.floor(larger(delay1, delay2) / 2);
+            let deltaDelay = Math.floor((largerDelay - smallerDelay)/2);
 
             let count = Math.floor((input.timing[i] * 5 * tempo) / smallerDelay);
 
@@ -105,8 +106,8 @@ module.exports = (input) => {
                 data.action.push(1);
                 data.action.push(1);
 
-                data.delay.push(smallerDelay / 2);
-                data.delay.push((largerDelay - smallerDelay) / 2);
+                data.delay.push(smallerDelay);
+                data.delay.push(deltaDelay);
 
                 data.motor.push(notemap.get(sample[0]).m - 1);
                 data.motor.push(notemap.get(sample[1]).m - 1);
@@ -116,8 +117,8 @@ module.exports = (input) => {
                 data.action.push(0);
                 data.action.push(0);
 
-                data.delay.push(smallerDelay /2);
-                data.delay.push((largerDelay - smallerDelay) / 2);
+                data.delay.push(smallerDelay);
+                data.delay.push(deltaDelay);
                 
                 data.motor.push(notemap.get(sample[0]).m - 1);
                 data.motor.push(notemap.get(sample[1]).m - 1);
