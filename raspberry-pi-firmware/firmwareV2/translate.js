e4ba6f4a6730dee3b922800abbf9c25ec8cdb65e
AdBol05 <adam.b@volny.cz>
2022-11-07 09:23:18 +0100

    cleared

--- raspberry-pi-firmware/firmwareV2/translate.js
+++ raspberry-pi-firmware/firmwareV2/translate.js
@@ -11,13 +11,13 @@ module.exports = (input) => {
     notemap.set("ff", {ntm: 1351, m: 3});
     notemap.set("gf", {ntm: 1203, m: 4});
     notemap.set("af", {ntm: 1072, m: 5});
-    
+
     notemap.set("cf1", {ntm: 902, m: 6});
     notemap.set("df1", {ntm: 803, m: 7});
     notemap.set("ff1", {ntm: 675, m: 8});
     notemap.set("gf1", {ntm: 601, m: 9});
     notemap.set("af1", {ntm: 536, m: 10});
-    
+
     notemap.set("c0", {ntm: 3823, m: 1});
     notemap.set("d0", {ntm: 3214, m: 2});
     notemap.set("e0", {ntm: 3034, m: 3});
@@ -25,7 +25,7 @@ module.exports = (input) => {
     notemap.set("g0", {ntm: 2550, m: 5});
     notemap.set("a0", {ntm: 2273, m: 6});
     notemap.set("b0", {ntm: 2024, m: 7});
-    
+
     notemap.set("c", {ntm: 1912, m: 8});
     notemap.set("d", {ntm: 1703, m: 9});
     notemap.set("e", {ntm: 1517, m: 10});
@@ -33,7 +33,7 @@ module.exports = (input) => {
     notemap.set("g", {ntm: 1276, m: 12});
     notemap.set("a", {ntm: 1136, m: 1});
     notemap.set("b", {ntm: 1012, m: 2});
-    
+
     notemap.set("c1", {ntm: 956, m: 3});
     notemap.set("d1", {ntm: 851, m: 4});
     notemap.set("e1", {ntm: 758, m: 5});
@@ -41,7 +41,7 @@ module.exports = (input) => {
     notemap.set("g1", {ntm: 637, m: 7});
     notemap.set("a1", {ntm: 568, m: 8});
     notemap.set("b1", {ntm: 506, m: 9});
-    
+
     notemap.set("c2", {ntm: 478, m: 10});
     notemap.set("d2", {ntm: 426, m: 11});
     notemap.set("e2", {ntm: 379, m: 12});
@@ -49,7 +49,7 @@ module.exports = (input) => {
     notemap.set("g2", {ntm: 319, m: 2});
     notemap.set("a2", {ntm: 284, m: 3});
     notemap.set("b2", {ntm: 253, m: 4});
-    
+
     notemap.set("af0", {ntm: 2144, m: 5});
 
     console.log("Processing input...");
@@ -91,7 +91,7 @@ module.exports = (input) => {
             let delay2 = Math.floor(notemap.get(sample[1]).ntm * oct);
             let smallerDelay = Math.floor(smaller(delay1, delay2));
             let largerDelay = Math.floor(larger(delay1, delay2)).toFixed();
-            
+
             let count = Math.floor((input.timing[i] * 5 * tempo) / smallerDelay);
 
             smallerDelay = Math.floor(smallerDelay/2);
@@ -107,7 +107,7 @@ module.exports = (input) => {
                     }
                     else{
                         data.motor.push(notemap.get(sample[1]).m - 1);
-                        data.motor.push(notemap.get(sample[0]).m - 1);                    
+                        data.motor.push(notemap.get(sample[0]).m - 1);
                     }
 
                     data.action.push(k);
