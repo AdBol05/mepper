e34a9a124720ecbd5a9168b2952bb72d968b4c70
AdBol05 <adam.b@volny.cz>
2022-11-10 19:01:08 +0100

    more values

--- raspberry-pi-firmware/firmwareV2/translate.js
+++ raspberry-pi-firmware/firmwareV2/translate.js
@@ -123,16 +123,19 @@ module.exports = (input) => {
             ]
             
             for(let h = 0; h < 2; h++){
-
+                
                 for(let j = 0; j < count[h]; j++){
-
+                    let del = 0;
+                    
                     for(let k = 0; k < 2; k++){
                         seq[h].motor.push(motor[h]);
                         seq[h].action.push(k);
-                        if(j === 0){seq[h].timestamp.push(0);}
-                        else{seq[h].timestamp.push(seq[h].timestamp[j - 1] + delay[h]);}
-                        seq[h].delay.push(delay[h]);
-                        console.log(seq[h].timestamp[j - 1] + delay[h]);
+                        //if(j === 0){seq[h].timestamp.push(0);}
+                        //else{seq[h].timestamp.push(seq[h].timestamp[j - 1] + delay[h]);}
+                        //seq[h].delay.push(delay[h]);
+                        del = del + delay[h];
+                        seq[h].timestamp.push(del);
+                        console.log(del);
                     }
 
                 }
